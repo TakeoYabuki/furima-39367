c6903a0762724d8b7c310475f7736d49948ac3a1
TakeoYabuki <70153768+TakeoYabuki@users.noreply.github.com>
2023-06-03 14:55:31 +0900

    テスト前

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
-
-  validates :nickname,               presence: true
-  validates :email,                  presence: true
-  validates :encrypted_password,     presence: true
-  validates :first_name,             presence: true
-  validates :last_name,              presence: true
-  validates :first_name_kana,        presence: true
-  validates :last_name_kana,         presence: true
-  validates :birthday,               presence: true
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/, message: "should contain a combination of letters and numbers" }
+  validates :first_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}ー]+\z/, message: "should be full-width characters" }
+  validates :last_name, presence: true, format: { with: /\A[\p{Hiragana}\p{Katakana}\p{Han}ー]+\z/, message: "should be full-width characters" }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Mn}ー]+\z/, message: "should be full-width katakana characters" }
+  validates :last_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Mn}ー]+\z/, message: "should be full-width katakana characters" }
+  validates :birthday, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
