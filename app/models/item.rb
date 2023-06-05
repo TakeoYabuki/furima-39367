11b9f2d9d61fcaf35ce00dafb60d32455c1d0c02
TakeoYabuki <70153768+TakeoYabuki@users.noreply.github.com>
2023-06-06 01:26:12 +0900

    出品商品のテーブル保存が可能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,13 @@
 class Item < ApplicationRecord
-has_one_attached :image
+  has_one_attached :image
 
-extend ActiveHash::Associations::ActiveRecordExtensions
-belongs_to :category 
-belongs_to :condition 
-belongs_to :postage 
-belongs_to :prefecture
-belongs_to :delivery_day 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category 
+    belongs_to :condition 
+    belongs_to :postage 
+    belongs_to :prefecture
+    belongs_to :delivery_day 
 
-validates :name, :explanation, :price,  presence: true
-
-validates :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id,  numericality: { other_than: 1, message: "can't be blank"}
+    validates :name, :explanation, :price,  presence: true
+    validates :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id,  numericality: { other_than: 1, message: "can't be blank"}
 end
