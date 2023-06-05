7dcb34b6bb9c76fe86d49460c1683d397ae88d5f
TakeoYabuki <70153768+TakeoYabuki@users.noreply.github.com>
2023-06-05 21:00:02 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
- has_one_attached :image
+has_one_attached :image
 
- extend ActiveHash::Associations::ActiveRecordExtensions
- belongs_to :category, :condition, :postage, :prefecture, :delivery_day 
+extend ActiveHash::Associations::ActiveRecordExtensions
+belongs_to :category 
+belongs_to :condition 
+belongs_to :postage 
+belongs_to :prefecture
+belongs_to :delivery_day 
+
+validates :name, :explanation, :price,  presence: true
+
+validates :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id,  numericality: { other_than: 1, message: "can't be blank"}
 end
