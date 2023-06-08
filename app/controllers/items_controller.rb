3864c0ae773e732c7757694591a72f105f2b296d
TakeoYabuki <70153768+TakeoYabuki@users.noreply.github.com>
2023-06-08 20:11:42 +0900

    指摘箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,16 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, except: [:index, :new, :create]
   before_action :move_to_index, except: [:index, :show, :new, :create]
 
-  
   def index
     @items = Item.order(created_at: :desc)
   end 
-  
+
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -21,34 +21,32 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end  
 
   def edit
-  @item = Item.find(params[:id])
-
   end
 
-  
   def update
-    @item = Item.find(params[:id])
-   if @item.update(item_params)
-     redirect_to item_path(@item)
-   else
-     render :edit
-   end
- end
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
 
-  
   private
-    def item_params
-      params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
-    end
 
-    def move_to_index
-      @item = Item.find(params[:id])
-      unless current_user == @item.user
-        redirect_to root_path
-      end
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless current_user == @item.user
+      redirect_to root_path
     end
-end  
+  end
+end
